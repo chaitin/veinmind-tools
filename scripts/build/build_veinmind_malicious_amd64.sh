f5b74400f51d1d00ccc8358883200081fec6c945
d_infinite <jingyuan.chen@chaitin.com>
2022-02-23 14:42:31 +0800

    feat: auto-release

--- scripts/build/build_veinmind_malicious_amd64.sh
+++ scripts/build/build_veinmind_malicious_amd64.sh
@@ -1,4 +1,4 @@
 go mod tidy
 mkdir -p ./artifacts/${CI_GOOS}-${CI_GOARCH}
 export CGO_ENABLED=1 GOOS="$CI_GOOS" GOARCH="$CI_GOARCH"
-go build -a -tags community -o ./artifacts/${CI_GOOS}-${CI_GOARCH}/veinmind-malicious ./cmd/scan/
+go build -a -tags community -o ./artifacts/${CI_GOOS}-${CI_GOARCH}/veinmind-malicious_${CI_GOOS}_${CI_GOARCH} ./cmd/scan/
