ed92df7da9cc7f77096472c409434b86b4db5ba8
d_infinite <jingyuan.chen@chaitin.com>
2022-03-24 19:06:50 +0800

    feat(veinmind-runner): registry

--- veinmind-runner/cmd/cli.go
+++ veinmind-runner/cmd/cli.go
@@ -2,15 +2,20 @@ package main
 
 import (
 	"context"
+	"errors"
 	"github.com/chaitin/libveinmind/go"
 	"github.com/chaitin/libveinmind/go/cmd"
+	"github.com/chaitin/libveinmind/go/docker"
 	"github.com/chaitin/libveinmind/go/plugin"
 	"github.com/chaitin/libveinmind/go/plugin/log"
 	"github.com/chaitin/libveinmind/go/plugin/service"
 	"github.com/chaitin/veinmind-tools/veinmind-common/go/service/report"
+	"github.com/chaitin/veinmind-tools/veinmind-runner/pkg/registry"
 	"github.com/spf13/cobra"
+	"io/ioutil"
 	"os"
 	"path"
+	"strings"
 )
 
 var (
@@ -19,16 +24,9 @@ var (
 	reportService = func() *report.ReportService {
 		return report.NewReportService()
 	}()
-)
-
-var rootCmd = &cmd.Command{}
-var scanCmd = &cmd.Command{
-	Use:   "scan",
-	Short: "perform hosted scan command",
-	PreRunE: func(c *cobra.Command, args []string) error {
+	scanPreRunE = func(c *cobra.Command, args []string) error {
 		// Discover Plugins
 		ctx = c.Context()
-		log.Info("Start discovering")
 		glob, err := c.Flags().GetString("glob")
 		if err == nil && glob != "" {
 			ps, err = plugin.DiscoverPlugins(ctx, ".", plugin.WithGlob(glob))
@@ -41,7 +39,6 @@ var scanCmd = &cmd.Command{
 		for _, p := range ps {
 			log.Infof("Discovered plugin: %#v\n", p.Name)
 		}
-		log.Info("Done discovering")
 
 		// Event Channel Listen
 		go func() {
@@ -54,10 +51,174 @@ var scanCmd = &cmd.Command{
 		}()
 
 		return nil
+	}
+)
+
+var rootCmd = &cmd.Command{}
+var scanHostCmd = &cmd.Command{
+	Use:     "scan-host",
+	Short:   "perform hosted scan command",
+	PreRunE: scanPreRunE,
+}
+var scanRegistryCmd = &cmd.Command{
+	Use:     "scan-registry",
+	Short:   "perform registry scan command",
+	PreRunE: scanPreRunE,
+	RunE: func(cmd *cobra.Command, args []string) error {
+		address, _ := cmd.Flags().GetString("address")
+		username, _ := cmd.Flags().GetString("username")
+		password, _ := cmd.Flags().GetString("password")
+		namespace, _ := cmd.Flags().GetString("namespace")
+		reponames, _ := cmd.Flags().GetStringSlice("reponames")
+		tags, _ := cmd.Flags().GetStringSlice("tags")
+
+		auth := &registry.Auth{}
+		if username != "" && password != "" {
+			auth.Username = username
+			auth.Password = password
+		} else {
+			auth = nil
+		}
+
+		client, err := registry.NewRegistryClient(address, auth)
+		if err != nil {
+			return err
+		}
+
+		// If no repo is specified, then query all repo through catalog
+		repos := []string{}
+		if len(reponames) == 0 {
+			repos, err = client.GetRepos()
+			if err != nil {
+				return err
+			}
+		} else {
+			// If it doesn't start with registry, autofill registry
+			for _, r := range reponames {
+				rSplit := strings.Split(r, "/")
+				rNew := ""
+				if !strings.EqualFold(rSplit[0], address) {
+					rSplitNew := []string{address}
+					rSplitNew = append(rSplitNew, rSplit...)
+					rNew = strings.Join(rSplitNew, "/")
+				} else {
+					rNew = r
+				}
+				repos = append(repos, rNew)
+			}
+		}
+
+		if namespace != "" {
+			namespaceMaps := map[string][]string{}
+			for _, repo := range repos {
+				repoSplit := strings.Split(repo, "/")
+				if len(repoSplit) >= 3 {
+					namespace := repoSplit[1]
+					namespaceMaps[namespace] = append(namespaceMaps[namespace], repo)
+				} else if len(repoSplit) == 2 {
+					namespace := repoSplit[0]
+					namespaceMaps[namespace] = append(namespaceMaps[namespace], repo)
+				} else if len(repoSplit) == 1 {
+					namespaceMaps["_"] = append(namespaceMaps["_"], repo)
+				}
+			}
+
+			_, ok := namespaceMaps[namespace]
+			if ok {
+				repos = namespaceMaps[namespace]
+			} else {
+				return errors.New("Namespace doesn't match any repos")
+			}
+		}
+
+		if len(tags) > 0 {
+			reposTemp := []string{}
+			for _, repo := range repos {
+				rtags, err := client.GetRepoTags(repo)
+				if err != nil {
+					log.Error(err)
+					continue
+				}
+
+				for _, t1 := range rtags {
+					for _, t2 := range tags {
+						if strings.EqualFold(t1, t2) {
+							repoSplit := strings.Split(repo, ":")
+							if len(repoSplit) == 1 {
+								repoSplit = append(repoSplit, t1)
+								repoWithTag := strings.Join(repoSplit, ":")
+								reposTemp = append(reposTemp, repoWithTag)
+							}
+						}
+					}
+				}
+			}
+			repos = reposTemp
+		}
+
+		d, err := docker.New()
+		if err != nil {
+			return err
+		}
+		defer func() {
+			d.Close()
+		}()
+
+		for _, repo := range repos {
+			log.Infof("Start pull image: %#v\n", repo)
+			r, err := client.Pull(repo)
+			if err != nil {
+				log.Errorf("Pull image error: %#v\n", err.Error())
+				continue
+			}
+
+			_, err = ioutil.ReadAll(r)
+			if err != nil {
+				log.Errorf("Pull image error: %#v\n", err.Error())
+				continue
+			}
+			log.Infof("Pull image success: %#v\n", repo)
+			ids, err := d.FindImageIDs(repo)
+			defer func() {
+				for _, id := range ids {
+					_, err := client.Remove(id)
+					if err != nil {
+						log.Error(err)
+					}
+					log.Infof("Remove image success: %#v\n", repo)
+				}
+			}()
+
+			if len(ids) > 0 {
+				for _, id := range ids {
+					image, err := d.OpenImageByID(id)
+					if err != nil {
+						log.Error(err)
+						continue
+					}
+
+					err = scan(cmd, image)
+					if err != nil {
+						log.Error(err)
+						continue
+					}
+				}
+			}
+		}
+
+		return nil
 	},
 }
 
 func scan(c *cmd.Command, image api.Image) error {
+	refs, err := image.RepoRefs()
+	ref := ""
+	if err == nil && len(refs) > 0 {
+		ref = refs[0]
+	} else {
+		ref = image.ID()
+	}
+	log.Infof("Scan image: %#v\n", ref)
 	if err := cmd.ScanImages(ctx, ps, []api.Image{image},
 		plugin.WithExecInterceptor(func(
 			ctx context.Context, plug *plugin.Plugin, c *plugin.Command,
@@ -80,8 +241,15 @@ func scan(c *cmd.Command, image api.Image) error {
 }
 
 func init() {
-	rootCmd.AddCommand(cmd.MapImageCommand(scanCmd, scan))
-	scanCmd.Flags().StringP("glob", "g", "", "specifies the pattern of plugin file to find")
+	rootCmd.AddCommand(cmd.MapImageCommand(scanHostCmd, scan))
+	rootCmd.AddCommand(scanRegistryCmd)
+	scanHostCmd.Flags().StringP("glob", "g", "", "specifies the pattern of plugin file to find")
+	scanRegistryCmd.Flags().StringP("address", "a", "index.docker.io", "server address of registry")
+	scanRegistryCmd.Flags().StringP("username", "u", "", "username of registry")
+	scanRegistryCmd.Flags().StringP("password", "p", "", "password of registry")
+	scanRegistryCmd.Flags().StringP("namespace", "n", "", "namespace of repo")
+	scanRegistryCmd.Flags().StringSliceP("reponames", "r", []string{}, "name of repo")
+	scanRegistryCmd.Flags().StringSliceP("tags", "t", []string{"latest"}, "tags of repo")
 }
 
 func main() {
--- veinmind-runner/go.mod
+++ veinmind-runner/go.mod
@@ -2,13 +2,21 @@ module github.com/chaitin/veinmind-tools/veinmind-runner
 
 go 1.16
 
-replace (
-	github.com/chaitin/veinmind-tools/veinmind-common/go v1.0.0 => ../veinmind-common/go
-)
+replace github.com/chaitin/veinmind-tools/veinmind-common/go v1.0.0 => ../veinmind-common/go
 
 require (
+	github.com/Microsoft/go-winio v0.5.2 // indirect
 	github.com/chaitin/libveinmind v1.0.4
-	github.com/sirupsen/logrus v1.8.1 // indirect
-	golang.org/x/sys v0.0.0-20211216021012-1d35b9e2eb4e // indirect
 	github.com/chaitin/veinmind-tools/veinmind-common/go v1.0.0
+	github.com/containerd/containerd v1.6.1 // indirect
+	github.com/docker/cli v20.10.12+incompatible
+	github.com/docker/distribution v2.8.1+incompatible // indirect
+	github.com/docker/docker v20.10.13+incompatible
+	github.com/fvbommel/sortorder v1.0.2 // indirect
+	github.com/google/go-containerregistry v0.8.0
+	github.com/moby/sys/mount v0.3.1 // indirect
+	github.com/moby/term v0.0.0-20210619224110-3f7ff695adc6 // indirect
+	github.com/spf13/cobra v1.3.0
+	github.com/theupdateframework/notary v0.7.0 // indirect
+	gotest.tools/v3 v3.1.0 // indirect
 )
