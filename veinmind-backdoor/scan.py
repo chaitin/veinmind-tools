ec6408d39a77c082e9f85a5edb74844f5a9360b9
d_infinite <463394797@qq.com>
2022-03-28 17:22:51 +0800

    refactor(veinmind-backdoor): adapt-python-plugin-system

--- veinmind-backdoor/scan.py
+++ veinmind-backdoor/scan.py
@@ -9,39 +9,40 @@ from common import tools
 from veinmind import *
 from plugins import *
 
+results = []
+start = 0
+image_ids = []
 
-@click.command()
-@click.option('--engine',default="docker", help="engine type you use, e.g. docker/containerd")
-@click.option('--name', default="", help="image name e.g. ubuntu:latest")
+@command.group()
 @click.option('--format', default="stdout", help="output format e.g. stdout/json")
 @click.option('--output', default='.', help="output path e.g. /tmp")
-def cli(engine, name, format, output):
-    results = []
-
+def cli(format, output):
+    global start
     start = time.time()
-    with runtime(engine) as client:
-        if name != "":
-            image_ids = client.find_image_ids(name)
-        else:
-            image_ids = client.list_image_ids()
-        for id in image_ids:
-            image = client.open_image_by_id(id)
-            if len(image.reporefs()) > 0:
-                log.logger.info("start scan: " + image.reporefs()[0])
-            else:
-                log.logger.info("start scan: " + image.id())
-            for plugin_name, plugin in register.register.plugin_dict.items():
-                p = plugin()
-                for r in p.detect(image):
-                    results.append(r)
+    pass
+
+
+@cli.image_command()
+def scan_images(image):
+    """scan image backdoor"""
+    global image_ids
+    image_ids.append(image.id())
+    if len(image.reporefs()) > 0:
+        log.info("start scan: " + image.reporefs()[0])
+    else:
+        log.info("start scan: " + image.id())
+    for plugin_name, plugin in register.register.plugin_dict.items():
+        p = plugin()
+        for r in p.detect(image):
+            results.append(r)
+
+@cli.resultcallback()
+def callback(result, format, output):
     spend_time = time.time() - start
 
-    if format == "stdout" and len(results) > 0:
+    if format == "stdout":
         print("# ================================================================================================= #")
-        image_id_dict = {}
-        for r in results:
-            image_id_dict[r.image_id] = 0
-        tools.tab_print("Scan Image Total: " + str(len(image_id_dict)))
+        tools.tab_print("Scan Image Total: " + str(len(image_ids)))
         tools.tab_print("Spend Time: " + spend_time.__str__() + "s")
         tools.tab_print("Backdoor Total: " + str(len(results)))
         for r in results:
@@ -57,13 +58,7 @@ def cli(engine, name, format, output):
         with open(fpath, mode="w") as f:
             f.write(jsonpickle.dumps(results))
 
-def runtime(engine):
-    if engine == "docker":
-        return docker.Docker()
-    elif engine == "containerd":
-        return containerd.Containerd()
-    else:
-        raise Exception("engine type doesn't match")
 
 if __name__ == '__main__':
+    cli.add_info_command()
     cli()
