31731298c14f5e493d8da39df2c78394a1537e51
d_infinite <463394797@qq.com>
2022-04-24 11:32:31 +0800

    chore(veinmind-runner): fix script

--- veinmind-runner/parallel-container-run.sh
+++ veinmind-runner/parallel-container-run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run --rm -it --mount 'type=bind,source=/,target=/host,readonly,bind-propagation=rslave'-v /var/run/docker.sock:/var/run/docker.sock veinmind/veinmind-runner $*
+docker run --rm -it --mount 'type=bind,source=/,target=/host,readonly,bind-propagation=rslave' -v /var/run/docker.sock:/var/run/docker.sock veinmind/veinmind-runner $*
