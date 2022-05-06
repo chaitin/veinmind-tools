2fff78b6f0fe552f8b6e101803dac04e216278eb
d_infinite <463394797@qq.com>
2022-05-06 18:23:21 +0800

    feat(veinmind-malicious): run-clamd-in-container

--- veinmind-malicious/parallel-container-run.sh
+++ veinmind-malicious/parallel-container-run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run --rm -it --network veinmind --mount 'type=bind,source=/,target=/host,readonly,bind-propagation=rslave' -v `pwd`:/tool/data -e CLAMD_HOST=clamav -e CLAMD_PORT=3310 veinmind/veinmind-malicious $*
+docker run --rm -it --mount 'type=bind,source=/,target=/host,readonly,bind-propagation=rslave' -v `pwd`:/tool/data veinmind/veinmind-malicious $*
