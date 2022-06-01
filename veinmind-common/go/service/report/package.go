fe631efe8a7b855ef4afff89986f05777ddbd10e
d_infinite <463394797@qq.com>
2022-06-01 19:20:11 +0800

    feat(veinmind-common): update asset detail

--- veinmind-common/go/service/report/package.go
+++ veinmind-common/go/service/report/package.go
@@ -109,25 +109,42 @@ type HistoryDetail struct {
 }
 
 type AssetDetail struct {
-	Type       string `json:"type"`
-	Name       string `json:"name"`
-	Version    string `json:"version"`
-	Release    string `json:"release"`
-	Epoch      int    `json:"epoch"`
-	Arch       string `json:"arch"`
-	SrcName    string `json:"srcName"`
-	SrcVersion string `json:"srcVersion"`
-	SrcRelease string `json:"srcRelease"`
-	SrcEpoch   int    `json:"srcEpoch"`
+	OS           AssetOSDetail         `json:"os"`
+	PackageInfos []AssetPackageDetails `json:"package_infos"`
+	Applications []AssetApplicationDetails `json:"applications"`
+}
 
-	Modularitylabel string `json:"modularitylabel"` // only for Red Hat based distributions
+type AssetOSDetail struct {
+	Family string `json:"family"`
+	Name   string `json:"name"`
+	Eosl   bool   `json:"EOSL,omitempty"`
+}
 
-	Indirect bool   `json:"indirect"`
-	License  string `json:"license"`
-	Layer    string `json:"layer"`
+type AssetPackageDetails struct {
+	FilePath string               `json:"file_path"`
+	Packages []AssetPackageDetail `json:"packages"`
+}
 
-	// Each package metadata have the file path, while the package from lock files does not have.
-	FilePath string `json:"filePath"`
+type AssetApplicationDetails struct {
+	Type     string               `json:"type"`
+	FilePath string               `json:"file_path,omitempty"`
+	Packages []AssetPackageDetail `json:"packages"`
+}
+
+type AssetPackageDetail struct {
+	Name            string `json:"name"`
+	Version         string `json:"version"`
+	Release         string `json:"release"`
+	Epoch           int    `json:"epoch"`
+	Arch            string `json:"arch"`
+	SrcName         string `json:"srcName"`
+	SrcVersion      string `json:"srcVersion"`
+	SrcRelease      string `json:"srcRelease"`
+	SrcEpoch        int    `json:"srcEpoch"`
+	Modularitylabel string `json:"modularitylabel"`
+	Indirect        bool   `json:"indirect"`
+	License         string `json:"license"`
+	Layer           string `json:"layer"`
 }
 
 type BasicDetail struct {
