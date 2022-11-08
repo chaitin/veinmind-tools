1336bb36ae4abc232a7804c99dbce61767ebc624
d_infinite <463394797@qq.com>
2022-11-08 17:29:12 +0800

    feat(unsafe-mount): rules (#151)

--- plugins/go/veinmind-unsafe-mount/pkg/engine/unsafe.go
+++ plugins/go/veinmind-unsafe-mount/pkg/engine/unsafe.go
@@ -1,13 +1,24 @@
 package engine
 
 var UnsafeMountPaths = []string{
+	// system directory
 	"/",
+	"/root",
 	"/etc",
 	"/boot",
 	"/var",
 	"/proc",
 	"/bin",
 	"/sys",
+
+	// runtime socket
 	"/var/run/docker.sock",
 	"/run/containerd.sock",
+	"/var/run/crio/crio.sock",
+
+	// kubernetes
+	"/var/lib/kubelet",
+	"/var/lib/kubelet/pki",
+	"/etc/kubernetes",
+	"/etc/kubernetes/manifests",
 }
