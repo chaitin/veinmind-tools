f0b1f90405e988fecb0fb708116ef0c49caeeaec
DVKunion <dvkunion@gmail.com>
2023-04-28 10:28:45 +0800

    fix: walk error

--- plugins/go/veinmind-minio/pkg/scanner/scanner.go
+++ plugins/go/veinmind-minio/pkg/scanner/scanner.go
@@ -17,9 +17,9 @@ const REGEX = `RELEASE\.(\d{4}-\d{2}-\d{2})T(\d{2}-\d{2}-\d{2})Z`
 var startVersion, _ = time.Parse("2006-01-02", "2019-12-17")
 var endVersion, _ = time.Parse("2006-01-02", "2023-03-20")
 
-func ScanImage(image api.Image) (Result, error) {
+func ScanImage(image api.Image) Result {
 	res := Result{}
-	err := image.Walk("/", func(path string, info fs.FileInfo, err error) error {
+	image.Walk("/", func(path string, info fs.FileInfo, err error) error {
 		if err != nil {
 			log.Debug(err)
 			return nil
@@ -33,18 +33,21 @@ func ScanImage(image api.Image) (Result, error) {
 			r := regexp.MustCompile(REGEX)
 			file, err := image.Open(path)
 			if err != nil {
-				return err
+				log.Debug(err)
+				return nil
 			}
 			data, err := io.ReadAll(file)
 			if err != nil {
-				return err
+				log.Debug(err)
+				return nil
 			}
 			matches := r.FindAllStringSubmatch(string(data), -1)
 			if len(matches) > 0 && len(matches[len(matches)-1]) > 1 {
 				version := matches[len(matches)-1][1]
 				v, err := time.Parse("2006-01-02", version)
 				if err != nil {
-					return err
+					log.Debug(err)
+					return nil
 				}
 				if v.Equal(startVersion) || (v.Before(endVersion) && v.After(startVersion)) {
 					res.File = path
@@ -56,12 +59,12 @@ func ScanImage(image api.Image) (Result, error) {
 		return nil
 	})
 
-	return res, err
+	return res
 }
 
-func ScanContainer(container api.Container) (Result, error) {
+func ScanContainer(container api.Container) Result {
 	res := Result{}
-	err := container.Walk("/", func(path string, info fs.FileInfo, err error) error {
+	container.Walk("/", func(path string, info fs.FileInfo, err error) error {
 		if err != nil {
 			log.Debug(err)
 			return nil
@@ -76,18 +79,21 @@ func ScanContainer(container api.Container) (Result, error) {
 			r := regexp.MustCompile(REGEX)
 			file, err := container.Open(path)
 			if err != nil {
-				return err
+				log.Debug(err)
+				return nil
 			}
 			data, err := io.ReadAll(file)
 			if err != nil {
-				return err
+				log.Debug(err)
+				return nil
 			}
 			matches := r.FindAllStringSubmatch(string(data), -1)
 			if len(matches) > 0 && len(matches[len(matches)-1]) > 1 {
 				version := matches[len(matches)-1][1]
 				v, err := time.Parse("2006-01-02", version)
 				if err != nil {
-					return err
+					log.Debug(err)
+					return nil
 				}
 				if v.Equal(startVersion) || (v.Before(endVersion) && v.After(startVersion)) {
 					res.File = path
@@ -98,5 +104,5 @@ func ScanContainer(container api.Container) (Result, error) {
 		}
 		return nil
 	})
-	return res, err
+	return res
 }
